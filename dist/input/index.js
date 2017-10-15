d05f63ae445c8f81bd4a4adc6b23e24ade14ceff
anton <anton.koch@gmail.com>
2017-10-15 04:44:09 +0200

    rebuilt dist

--- dist/input/index.js
+++ dist/input/index.js
@@ -1,17 +1,12 @@
 'use strict';
 
 exports.__esModule = true;
-exports.MIDI = exports.CSV = undefined;
+exports.CSV = undefined;
 
 var _csv = require('./csv');
 
 var _csv2 = _interopRequireDefault(_csv);
 
-var _midi = require('./midi');
-
-var _midi2 = _interopRequireDefault(_midi);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.CSV = _csv2.default;
-exports.MIDI = _midi2.default;
