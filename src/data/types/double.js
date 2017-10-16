5d356cf675ea390b983eaaf838885bf34bcecc74
anton <anton.koch@gmail.com>
2017-10-16 02:30:05 +0200

    return null instead of undefined if fails

--- src/data/types/double.js
+++ src/data/types/double.js
@@ -4,7 +4,7 @@ BigNumber.config({ ERRORS: false })
 class Double extends BigNumber {
   static fromString (key) {
     if (typeof key === 'string' && key[0] === '+') key = key.substr(1)
-    return key ? new Double(key) : undefined
+    return key ? new Double(key) : null
   }
 }
 
